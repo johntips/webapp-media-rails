496d06e6b9e49425b7ac5c1b26a12c35140f2d52
Teiyu Ueki <tu.ue.et@gmail.com>
2015-12-02 05:02:27 +0900

    set gemfile path

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,6 @@
+#local setting
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+#production rails c setting
+# ENV['BUNDLE_GEMFILE'] ||= '/var/www/html/webapp-tabimuse-rails/current/Gemfile'
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
