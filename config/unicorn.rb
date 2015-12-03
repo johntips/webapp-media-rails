4a5d5fc21d94a78dbee061d49a9a9b672556dc5c
teiyuueki <tu.ue.et@gmail.com>
2015-12-03 13:27:26 +0900

    fix config/unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,32 +1,39 @@
 # -*- coding: utf-8 -*-
+
+
+shared_path = "/var/www/html/webapp-tabimuse-rails/shared"
+current_path =  "/var/www/html/webapp-tabimuse-rails/current"
+
+
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true  # 更新時ダウンタイム無し
+working_directory current_path
 
-listen "/tmp/unicorn.sock"
-pid "/tmp/unicorn.pid"
+listen File.expand_path('tmp/sockets/unicorn.sock', shared_path)
+pid File.expand_path('tmp/pids/unicorn.pid', shared_path)
+
+
 
 before_exec do |server, worker|
   ENV['BUNDLE_GEMFILE'] =  "/var/www/html/webapp-tabimuse-rails/current/Gemfile"
 end
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
+  ENV['BUNDLE_GEMFILE'] = File.expand_path('Gemfile', current_path)
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
   end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
 
 # ログの出力
